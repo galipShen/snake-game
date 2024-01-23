2ec6e677330d07e73e267f085e7f96b136ed6a79
Galip Sen <galipshen@gmail.com>
2024-01-23 17:13:27 +0300

    Game wrapped by PanGestureHandler and hadleGesture created

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { Colors } from "../styles/colors";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
 export default function Game(): JSX.Element {
-    return <SafeAreaView style={styles.container}></SafeAreaView>;
-}
+    const handleGesture = (event: any) => {
+        const { translationX, translationY } = event.nativeEvent
+        console.log(translationX, translationY)
+    }
 
+    return (
+        <PanGestureHandler onGestureEvent={handleGesture} >
+            <SafeAreaView style={styles.container} ></SafeAreaView>
+        </PanGestureHandler>
+    )
+}
+//<SafeAreaView style={styles.container} ></SafeAreaView>
 const styles = StyleSheet.create({
     container: {
         flex: 1,
