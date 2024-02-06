81eb6af360880f3bd4c3f72862b388f7f685b9b6
Galip Sen <galipshen@gmail.com>
2024-02-06 17:46:30 +0300

    snake skin fixed

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,6 +1,6 @@
 import { Fragment, JSXElementConstructor } from "react"
 import { Coordinate } from "../types/types"
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, Text } from "react-native"
 import { Colors } from "../styles/colors"
 import normalize from "react-native-normalize";
 
@@ -18,7 +18,10 @@ export default function Snake({ snake }: SnakeProps): JSX.Element {
                     left: segment.x * normalize(10),
                     top: segment.y * normalize(10)
                 };
-                return <View key={index} style={[styles.snake, segmentStyle]} />
+                return <View key={index} style={[styles.snake, segmentStyle]} >
+                    <Text style={styles.text}>||&%&½&#$#$#$#|&%$#ß*5*5*5*5*5567567567567567
+                    </Text>
+                </View>
             })}
 
         </Fragment>
@@ -27,10 +30,15 @@ export default function Snake({ snake }: SnakeProps): JSX.Element {
 
 const styles = StyleSheet.create({
     snake: {
-        width: normalize(15),
+        width: normalize(15),  //15
         height: normalize(15),
         backgroundColor: Colors.primary,
         borderRadius: normalize(7),
-        position: "absolute"
+        position: "absolute",
+    },
+    text: {
+        fontWeight: "900",
+        fontSize: 3,
     }
+
 })
